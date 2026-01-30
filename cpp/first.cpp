f8800e5274d08339d886eac6536dbcc519e02922
Xx0nothing0xX <naitik.20254096@mnnit.ac.in>
2026-01-30 16:25:29 +0530

    start cpp

---
+++ cpp/first.cpp
@@ -0,0 +1,37 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"Hello World im batman";       //to print smthing
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x; //declaration
+//     x=5;   //initialization
+//     int y=10;  //both
+//     int a=2,b=3;
+//     cout<< a*b <<endl;    //endl creates next line
+//     cout<<x+y;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=5;
+//     cout<< x <<endl;
+//     x=10;
+//     cout<< x ;
+// }
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x=5;
+    cout<< x <<endl;
+    x=10;
+    cout<< x ;
+}
+
---
+++ javascript/first.js
@@ -0,0 +1,12 @@
+console.log("hello world"); 
+console.log("Naitik jaiswal"); 
+
+// variables
+age = 19;
+fname = "naitik";
+a= null;
+b= undefined;
+price= 99.99;
+bool = true;
+console.log(fname); 
+
