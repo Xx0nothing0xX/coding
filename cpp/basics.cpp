fce2068af5a76b9c85a11ce627ef7faf284aa397
Xx0nothing0xX <naitik.20254096@mnnit.ac.in>
2026-01-31 13:10:50 +0530

    basics of cpp done

---
+++ cpp/basics.cpp
@@ -0,0 +1,118 @@
+//                                                         Print
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World im batman";       //to print smthing
+}
+
+
+//                                                            Variables
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x; //declaration
+//     x=5;   //initialization
+//     int y=10;  //both
+//     int a=2,b=3;
+//     cout<< a*b <<endl;                //endl creates next line
+//     cout<<x+y;
+// }
+
+
+//                                                                       overwrite
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=5;
+//     cout<< x <<endl;
+//     x=10;
+//     cout<< x ;
+// }
+
+// //                                                        arithmatic operations
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=5;
+//     x=x+5;       //x += 5
+//     cout<< x ;
+// }
+
+                                                 //  increament/decreament operators
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=5;
+//     x++;       //adds 1 to the value (first uses the value then adds 1 to it )
+// // x--   decreament
+//     cout<< x ;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=5;
+//     ++x;       //adds 1 to the value (first adds 1 to the value then uses the value)
+//     cout<< x ;
+// }
+
+// //                                                             floating data type
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float x=5;
+//     cout<<x/2;
+//     // if we took int x=5, ans would be 2 not 2.5 
+// }
+
+
+// //                                                             some characters var
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char a='n';
+//     cout<<a;
+// }
+
+//                                              area of a circle
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float r=5;
+//     cout<<"Area of the circle is : ";
+//     cout<<3.14*r*r;
+// }
+ 
+//                                                  user input
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float x;
+//     cout<<"Enter a number: ";
+//     cin>> x;
+//     cout<<"Square of the number is: "<< x*x;
+// } 
+
+//                                                        typecasting
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=5;
+//     float y = (float)x;
+//     cout<<y/2; 
+// } 
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char a='k';
+//     cout<<(int)a;         //gives ascii value
+// }
+
+
+
+
