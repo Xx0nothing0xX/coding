820a8a72892c6276a97dbddf479e62ea3c60c1c7
Xx0nothing0xX <naitik.20254096@mnnit.ac.in>
2026-02-01 18:44:24 +0530

    cpp conditionals studied

--- cpp/basics.cpp
+++ cpp/basics.cpp
@@ -7,6 +7,7 @@ int main(){
 }
 
 
+
 //                                                            Variables
 
 // #include<iostream>
--- cpp/conditionals.cpp
+++ cpp/conditionals.cpp
@@ -1,5 +1,67 @@
+// // 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cout<<"Enter number A: ";
+//     cin>>a;
+//     int b;
+//     cout<<"Enter number B: ";
+//     cin>>b;
+//     int c;
+//     cout<<"Enter number C: ";
+//     cin>>c;
+//     if ( a<c && b<c){
+//          cout<<"C ";
+//     }
+//     else if (a<b && b>c){
+//         cout<<"B";
+//     }
+//     else {
+//         cout<<"A"; 
+//     }
+// }
+
+
+//                      ternary operators
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter n: ";
+//     cin>> n;
+//     (n%2==0) ? cout<<"even" : cout<<"odd";
+// } 
+
+//                              switch operator
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"Hello World im batman";       //to print smthing
+    int n1;
+    cout<<"enter n1: ";
+    cin>>n1;
+    char op; 
+    cout<<"enter op: ";
+    cin>>op;
+    int n2;
+    cout<<"enter n2: ";
+    cin>>n2; 
+    switch (op){
+        case '+':
+            cout<< n1+n2 <<endl;
+            break;
+        case '-':
+            cout<< n1-n2 <<endl;
+            break;
+        case '/':
+            cout<< n1/n2 <<endl;
+            break;
+        case '*':
+            cout<< n1*n2 <<endl;
+            break;
+        default:
+            cout<<"invalid operator";
+    }
+
+    
 }
